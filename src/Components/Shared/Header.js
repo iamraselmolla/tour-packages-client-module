d4283d68023ec25cbe65fb5d5ccdb9b22f077ca4
iamraselmolla <raselmolla6336@gmail.com>
2022-11-08 18:42:56 +0600

    added login register logout condionally

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../UserContext/AuthProvicer';
 
 const Header = () => {
+    const {user} = useContext(AuthContext)
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -16,9 +18,9 @@ const Header = () => {
                         <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/">Home</NavLink>
                         <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/packages">Packages</NavLink>
                         <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/blog">Blog</NavLink>
-                        <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/add-a-package">Add a Package</NavLink>
-                        <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/login">Login</NavLink>
-                        <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/register">Register</NavLink>
+                       {user &&  <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/add-a-package">Add a Package</NavLink>}
+                        {!user && <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/login">Login</NavLink>}
+                        {!user && <NavLink className="text-decoration-none px-3 fw-bolder text-black" to="/register">Register</NavLink>}
                         <NavLink className="text-decoration-none px-3 fw-bolder text-black" >Logout</NavLink>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
