a0d636ecc0d0c1de485ca0e4cee2fd1bf3a4b9bd
iamraselmolla <raselmolla6336@gmail.com>
2022-11-09 00:28:59 +0600

    Posting comment and saving it into database

--- src/Components/Shared/Packages/PackageDetails.js
+++ src/Components/Shared/Packages/PackageDetails.js
@@ -4,10 +4,13 @@ import { FaBinoculars, FaStar, FaStarHalf, FaHotel, FaBus, FaLuggageCart, FaCale
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { AuthContext } from '../../UserContext/AuthProvicer';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const PackageDetails = () => {
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     const packageData = useLoaderData();
     const [allpackages, setpackages] = useState([])
     const { _id, name, img, description, price, ratings } = packageData
@@ -16,6 +19,30 @@ const PackageDetails = () => {
             .then(res => res.json())
             .then(data => setpackages(data))
     }, [])
+    const submitReview = (e) => {
+        e.preventDefault();
+        const author = e.target.reviewname.value;
+        const img = user?.img || 'blank.png';
+        const packageId = _id;
+        const email = user?.email;
+        const comments = e.target.comments.value;
+        const reviewInfo = { packageId, author, email, img, comments };
+
+        fetch('http://localhost:5000/post-review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(reviewInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast.success('Review shared successfully')
+                }
+            })
+    }
+
     return (
         <section className='py-5'>
             <div className="container">
@@ -52,6 +79,36 @@ const PackageDetails = () => {
                         </div>
                         <div className="tour-details mt-2">
                             <p> {description}</p>
+                        </div>
+                        <div className="review-and-see-reviews mt-3">
+                            <div className="reviews">
+
+                            </div>
+                            <div className="add-review">
+                                <Form onSubmit={submitReview}>
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <Form.Group className="mb-3" controlId="formBasicEmail22">
+                                                <Form.Label>Full name</Form.Label>
+                                                <Form.Control defaultValue={user?.displayName} name="reviewname" type="text" placeholder="Enter email" />
+                                            </Form.Group>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <Form.Group className="mb-3" controlId="formBasicEmail22">
+                                                <Form.Label>Email</Form.Label>
+                                                <Form.Control name="reviewemail" defaultValue={user?.email} readOnly type="email" />
+                                            </Form.Group>
+                                        </div>
+                                        <Form.Group className="mb-3" controlId="formBasicEmail23233">
+                                            <Form.Label>Comments</Form.Label>
+                                            <Form.Control name="comments" placeholder='Write your comments here' as="textarea" />
+                                        </Form.Group>
+                                    </div>
+                                    <Button className='w-100 bg-black fs-5 fw-bolder text-white py-3 ' type="submit">
+                                        Share Review
+                                    </Button>
+                                </Form>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-3 text-white rounded  bg-black">
